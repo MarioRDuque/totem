56ed7f5a7b5ec91a1cc6a868fbd4bd0eed9b5a80
Camachobermeo <joelbermeo452@gmail.com>
2020-11-04 23:14:49 -0500

    nise

--- ListarPersonas.php
+++ ListarPersonas.php
@@ -73,6 +73,30 @@ $personas = $sentencia->fetchAll(PDO::FETCH_OBJ);
 		</div>
 	</div>
 </div>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">¿Esta seguro que desea eliminar? </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-primary">Confirmar</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 </html>
