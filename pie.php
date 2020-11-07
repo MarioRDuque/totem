0014e39a665c436c75f50473f758eeaba236dee2
Camachobermeo <joelbermeo452@gmail.com>
2020-11-07 17:42:28 -0500

    mensaje de confirmacion

--- pie.php
+++ pie.php
@@ -1,8 +1,8 @@
 <footer class="page-footer font-small stylish-color-dark pt-4" style="bottom: 0; position: fixed; width: 100%;">
   <div class="footer-copyright text-center py-3 bg-light">
-    <img src="../utiles/logo1.jpg" class="rounded-circle" width="40" height="33">
-    <a href="https://www.checkseguro.com/">www.checkseguro.com </a>          -   
-    <img src="../utiles/icono_instagram.jpg" class="rounded-circle" width="58" height="43">@check_seguro          -
+    <img src="../utiles/logo1.jpg" class="rounded-circle" width="40" height="34">
+    <a href="https://www.checkseguro.com/">www.checkseguro.com </a>
+    <img src="../utiles/icono_instagram.jpg" class="rounded-circle" width="63" height="43">@check_seguro
     <img src="../utiles/icono_facebook.jpg" class="rounded-circle" width="50" height="40">Check Seguro
 
   </div>
