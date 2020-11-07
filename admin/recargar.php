228cf9953dc7be3d2eb3564c492e0706472a67a5
Camachobermeo <joelbermeo452@gmail.com>
2020-11-07 17:52:16 -0500

    mensaje de confirmacion

---
+++ admin/recargar.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="es">
+
+<?php include_once "encabezado.php" ?>
+<br>
+<br>
+<br>
+
+
+<body>
+  <br><br>
+  <div class="container">
+    <br>
+    <h1 class="text-center">Recargar</h1>
+    <br>
+    <form action="<?php exec("sudo sh /var/www/html/totem/admin/apaga.sh") ?>" method="POST" class="text-center">
+      <button type="submit" class="btn btn-success">Recargar</button>
+    </form>
+  </div>
+
+</body>
+<br><br><br>
+<?php include_once "../pie.php" ?>
+
+</html>
