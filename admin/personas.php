99da9b21d31be58746ce78499fc8c7d45f9a932b
Mario Mario <marioramos083@gmail.com>
2020-11-09 12:58:54 -0500

    registrar

--- admin/personas.php
+++ admin/personas.php
@@ -3,68 +3,67 @@
 <?php
 $fallo = false;
 if ($_REQUEST)
-  $fallo = $_REQUEST['fallo'];
+    $fallo = $_REQUEST['fallo'];
 ?>
 
 <body>
-
-  <?php
-  if ($fallo) {
-    echo "<div class='alert alert-danger' role='alert' id='alerta'>
-        <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-        <strong>Error!</strong>    RUT o RFID ingresado, ya exite.
-     </div>";
-  }
-  ?>
-<?php include_once "encabezado.php" ?>
-<br>
-<br>
-<br>
-<div class="container">
-    <div class="text-center">
-        <h1>Registrar Persona</h1>
-    </div>
+    <?php include_once "encabezado.php" ?>
     <br>
-    <form action="InsertarPersonas.php" method="POST">
-        <div class="form-row">
-            <div class="col-3"></div>
-            <div class="col-3">
-                <div class="form-group">
-                    <label for="nombre">Nombre</label>
-                    <input required name="nombre" class="form-control text-uppercase" id="nombre" placeholder="Nombre de la persona">
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="form-group">
-                    <label for="apellidos">Apellidos</label>
-                    <input required name="apellidos" class="form-control text-uppercase" id="apellidos" placeholder="Apellidos de la persona">
-                </div>
-            </div>
-            <div class="col-3"></div>
-        </div>
-        <div class="form-row">
-            <div class="col-3"></div>
-            <div class="col-3">
-                <div class="form-group">
-                    <label for="rut">RUT</label>
-                    <input required name="rut" class="form-control text-uppercase" id="rut" placeholder="RUT de la persona">
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="form-group">
-                    <label for="rfid">RFID</label>
-                    <input required name="rfid" class="form-control text-uppercase" id="rfid" placeholder="RFID de la persona">
-                </div>
-            </div>
-            <div class="col-3"></div>
-        </div>
+    <br>
+    <br>
+    <div class="container">
+        <?php
+        if ($fallo) {
+            echo "<div class='alert alert-danger' role='alert' id='alerta'>
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                <strong>Error!</strong>    RUT o RFID ingresado, ya exite.
+            </div>";
+                }
+        ?>
         <div class="text-center">
-            <button type="submit" class="btn btn-success">Guardar</button>
+            <h1>Registrar Persona</h1>
         </div>
+        <br>
+        <form action="InsertarPersonas.php" method="POST">
+            <div class="form-row">
+                <div class="col-3"></div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input required name="nombre" class="form-control text-uppercase" id="nombre" placeholder="Nombre de la persona">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="apellidos">Apellidos</label>
+                        <input required name="apellidos" class="form-control text-uppercase" id="apellidos" placeholder="Apellidos de la persona">
+                    </div>
+                </div>
+                <div class="col-3"></div>
+            </div>
+            <div class="form-row">
+                <div class="col-3"></div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="rut">RUT</label>
+                        <input required name="rut" class="form-control text-uppercase" id="rut" placeholder="RUT de la persona">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="rfid">RFID</label>
+                        <input required name="rfid" class="form-control text-uppercase" id="rfid" placeholder="RFID de la persona">
+                    </div>
+                </div>
+                <div class="col-3"></div>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-success">Guardar</button>
+            </div>
 
-    </form>
-</div>
-<br><br><br>
-<?php include_once "../pie.php" ?>
+        </form>
+    </div>
+    <br><br><br>
+    <?php include_once "../pie.php" ?>
 
 </html>
